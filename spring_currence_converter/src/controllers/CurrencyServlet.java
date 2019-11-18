b3ce6d6972bafee5f6e5537ad9de24efec64ed5f
dudq <dudq.xmt@gmail.com>
2019-11-18 13:38:27 +0700

    add currency_converter file

---
+++ spring_currence_converter/src/controllers/CurrencyServlet.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CurrencyServlet {
+
+    @GetMapping("/change")
+    public String convert() {
+        return "converter";
+    }
+
+    @GetMapping("/converter")
+    public String resultChange(@RequestParam float usd, @RequestParam float rate, Model model) {
+        float result = usd * rate;
+        model.addAttribute("result", result);
+        return "converter";
+    }
+}
